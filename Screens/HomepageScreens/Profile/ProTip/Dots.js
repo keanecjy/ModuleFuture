2c38a24a4e5a4ec26108d1c4f61fc7adcb64ebb6
keanecjy <e0425601@u.nus.edu>
2020-12-30 17:48:34 +0800

    Remove deprecated file

--- Screens/HomepageScreens/Profile/ProTip/Dots.js
+++ Screens/HomepageScreens/Profile/ProTip/Dots.js
@@ -1,30 +1,30 @@
-import React from "react";
-import Animated, { interpolate, Extrapolate } from "react-native-reanimated";
+import React from 'react'
+import Animated, { Extrapolate, interpolate } from 'react-native-reanimated'
 
 const Dots = ({ index, currentIndex }) => {
   const opacity = interpolate(currentIndex, {
     inputRange: [index - 1, index, index + 1],
     outputRange: [0.5, 1, 0.5],
     extrapolate: Extrapolate.CLAMP,
-  });
+  })
   const scale = interpolate(currentIndex, {
     inputRange: [index - 1, index, index + 1],
     outputRange: [1, 1.25, 1],
     extrapolate: Extrapolate.CLAMP,
-  });
+  })
   return (
     <Animated.View
       style={{
         opacity,
         transform: [{ scale }],
-        backgroundColor: "#FB5581",
+        backgroundColor: '#FB5581',
         width: 8,
         height: 8,
         borderRadius: 4,
         margin: 4,
       }}
-    ></Animated.View>
-  );
-};
+    />
+  )
+}
 
-export default Dots;
+export default Dots
